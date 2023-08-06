3e18911e79d80767b7f85fe05360830b1ec2f26b
sHiNka20 <123722238+sHiNka20@users.noreply.github.com>
2023-08-06 15:29:32 +0300

    Add files via upload

---
+++ task 4.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int temp = 2;
+
+while(temp <= num - 2)
+{
+    Console.Write(temp);
+    Console.Write(", ");
+    temp = temp + 2;
+}
+Console.Write(temp);
+Console.WriteLine(".");
