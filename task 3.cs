0022cd2decbb16eb7e768864b0839fc44a6c66ab
sHiNka20 <123722238+sHiNka20@users.noreply.github.com>
2023-08-06 15:17:05 +0300

    Add files via upload

---
+++ task 3.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число, чтобы проверить четность : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
