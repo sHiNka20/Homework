ddc58110a0a61a01160259470ec3775b67d3520c
sHiNka20 <123722238+sHiNka20@users.noreply.github.com>
2023-08-06 15:42:33 +0300

    Add files via upload

---
+++ task 5.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    int result = num % 10;
+    Console.Write("Третья цифра :");
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
